c0abcf86018acb3e971b42b397b990c5c827e275
Cameron Hu <chu@profluent.bio>
2024-10-02 23:12:11 +0000

    Processing scripts for basic metadata and data parsing working and tested

--- src/etl/process.py
+++ src/etl/process.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 
+
 def parse_metadata(data_unit_file: str) -> dict:
     """Parses metadata from an OAS file
 
@@ -10,7 +11,41 @@ def parse_metadata(data_unit_file: str) -> dict:
     Returns:
         dict: metadata of the file in dict
     """
-    metadata = ','.join(pd.read_csv(data_unit_file, nrows=0).columns)
+
+    metadata = ",".join(pd.read_csv(data_unit_file, nrows=0).columns)
     metadata = json.loads(metadata)
 
     return metadata
+
+
+def parse_sequence_data(data_unit_file: str) -> pd.DataFrame:
+    """Parses actual sequence data from an OAS file
+
+    Args:
+        data_unit_file (str): path to the OAS sequence file
+
+    Returns:
+        pd.DataFrame: Df with sequence information corresponding to the datafile
+    """
+
+    sequence_df = pd.read_csv(data_unit_file, header=1)
+    return sequence_df
+
+
+def parse_file(data_unit_file: str) -> tuple[dict, pd.DataFrame]:
+    """Parses a given OAS file and extracts both its metadata and sequence data
+
+    Args:
+        data_unit_file (str): path to the OAS sequence file
+
+    Returns:
+        tuple[dict, pd.DataFrame]: tuple of metadata and sequence data
+    """
+
+    metadata = parse_metadata(data_unit_file)
+    sequence_data = parse_sequence_data(data_unit_file)
+
+    return metadata, sequence_data
+
+
+def 
